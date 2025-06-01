bc3e013b89f755d93b3a04f0203c7ba7d64e00b8
srkn.dlgc <serkangithub@gmail.com>
2025-06-01 19:12:35 +0300

    problem solved

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Header from './components/header/Header.jsx';
+import Header from './components/header/Header';
 import Home from './components/home/Home';
 import About from './components/about/About';
 import İnfo from './components/about/İnfo';
